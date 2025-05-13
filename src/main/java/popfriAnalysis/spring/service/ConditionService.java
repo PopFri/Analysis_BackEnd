f9491d635ee6bc3c2a618303c4693470aad89f9f
ohige01 <ohige01@naver.com>
2025-05-13 09:44:00 +0900

    [FEAT] #3 분석 조건 추가 구현

--- src/main/java/popfriAnalysis/spring/service/ConditionService.java
+++ src/main/java/popfriAnalysis/spring/service/ConditionService.java
@@ -2,10 +2,35 @@ package popfriAnalysis.spring.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import popfriAnalysis.spring.converter.ConditionConverter;
+import popfriAnalysis.spring.domain.AnalysisCondition;
+import popfriAnalysis.spring.domain.AnalysisProcess;
 import popfriAnalysis.spring.repository.ConditionRepository;
+import popfriAnalysis.spring.web.dto.ConditionRequest;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ConditionService {
     private final ConditionRepository conditionRepository;
+
+    public Boolean addAnalysisCondition(AnalysisProcess process, List<ConditionRequest.ConditionDto> conditionDtoList){
+        List<AnalysisCondition> conditionList = ConditionConverter.conditionDtoToConditionValue(conditionDtoList).stream()
+                .map(operator -> AnalysisCondition.builder()
+                        .process(process)
+                        .operator(operator)
+                        .build())
+                .toList();
+
+        conditionRepository.saveAll(conditionList);
+
+        return true;
+    }
+
+    public void delAnalysisConditionAll(AnalysisProcess process){
+        List<AnalysisCondition> oldConditionList = conditionRepository.findByProcess(process);
+
+        conditionRepository.deleteAll(oldConditionList);
+    }
 }
--- src/main/java/popfriAnalysis/spring/web/controller/ConditionController.java
+++ src/main/java/popfriAnalysis/spring/web/controller/ConditionController.java
@@ -1,10 +1,18 @@
 package popfriAnalysis.spring.web.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import popfriAnalysis.spring.apiPayload.ApiResponse;
+import popfriAnalysis.spring.domain.AnalysisProcess;
 import popfriAnalysis.spring.service.ConditionService;
+import popfriAnalysis.spring.service.ProcessService;
+import popfriAnalysis.spring.web.dto.ColumnRequest;
+import popfriAnalysis.spring.web.dto.ConditionRequest;
 
 @RestController
 @RequestMapping("/api/v1/condition")
@@ -12,5 +20,14 @@ import popfriAnalysis.spring.service.ConditionService;
 @Tag(name = "Condition", description = "Condition 관련 API입니다.")
 public class ConditionController {
     private final ConditionService conditionService;
+    private final ProcessService processService;
 
+    @PostMapping("")
+    @Operation(summary = "분석 조건 추가", description = "프로세스 아이디와 추가할 조건을 입력받아 저장")
+    public ApiResponse<Boolean> addAnalysisColumn(@RequestBody ConditionRequest.AddAnalysisColumnDto request){
+        AnalysisProcess analysisProcess = processService.getProcess(request.getProcessId());
+
+        conditionService.delAnalysisConditionAll(analysisProcess);
+        return ApiResponse.onSuccess(conditionService.addAnalysisCondition(analysisProcess, request.getCondition()));
+    }
 }
