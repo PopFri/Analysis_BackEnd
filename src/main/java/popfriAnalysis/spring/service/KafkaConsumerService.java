0d14ffb4917e9c440e16ab9bd639b1dcea0c029f
ohige01 <ohige01@naver.com>
2025-05-30 16:36:29 +0900

    [FEAT] #9 kafka 주소 설정

--- src/main/java/popfriAnalysis/spring/service/KafkaConsumerService.java
+++ src/main/java/popfriAnalysis/spring/service/KafkaConsumerService.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 @Slf4j
 @Service
 public class KafkaConsumerService {
-    @KafkaListener(topics = "matomo-log", groupId = "consumer_group01")
+    @KafkaListener(topics = "matomo-log", groupId = "popfri_analysis_group1")
     public void consume(String message) throws IOException {
         System.out.printf("Consumed Message : %s%n", message);
     }
