c52156ac5d90a9b953e63d575495cbbeae89cef3
ohige01 <ohige01@naver.com>
2025-06-18 15:18:42 +0900

    [FEAT] #29 SSE 요청 url 추가

--- src/main/java/popfriAnalysis/spring/sse/SseController.java
+++ src/main/java/popfriAnalysis/spring/sse/SseController.java
@@ -8,8 +8,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import popfriAnalysis.spring.web.dto.ResultResponse;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Map;
 
 @RestController
 @Slf4j
@@ -17,6 +20,7 @@ import java.io.IOException;
 public class SseController {
 
     private final SseEmitters sseEmitters;
+    private final SseService sseService;
 
     @GetMapping(value = "/connect", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public ResponseEntity<SseEmitter> connect() {
@@ -26,6 +30,40 @@ public class SseController {
             emitter.send(SseEmitter.event()
                     .name("connect")
                     .data("connected!"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return ResponseEntity.ok(emitter);
+    }
+
+    @GetMapping(value = "/daily-activity", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public ResponseEntity<SseEmitter> getActivity() {
+        SseEmitter emitter = new SseEmitter(5 * 60 * 1000L);
+        sseEmitters.add(emitter);
+
+        ResultResponse.getDailyActivityDto dto = sseService.getDailyActivity();
+
+        try {
+            emitter.send(SseEmitter.event()
+                    .name("dailyActivity")
+                    .data(dto));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return ResponseEntity.ok(emitter);
+    }
+
+    @GetMapping(value = "/data-cnt-graph", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public ResponseEntity<SseEmitter> count() {
+        SseEmitter emitter = new SseEmitter(5 * 60 * 1000L);
+        sseEmitters.add(emitter);
+
+        Map<LocalDateTime, Long> dto = sseService.getDataCntGraph();
+
+        try {
+            emitter.send(SseEmitter.event()
+                    .name("dataCntGraph")
+                    .data(dto));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
